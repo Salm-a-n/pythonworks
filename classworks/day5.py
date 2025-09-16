127b5c8f7818194db6444e3155e5ed72691ea28d
Salm-a-n <salmanntechs@gmail.com>
2025-09-16 20:42:39 +0530

    classroom work

---
+++ classworks/day5.py
@@ -0,0 +1,25 @@
+pythonset= set(("salman", "alfariz", "joseph","ashik")) 
+print(pythonset)
+print(type(pythonset))
+dascset = {"salman","alfariz","joseph","ashik"}
+print(dascset)
+print(type(dascset))
+pythonset.add("samiya")
+dascset.remove("ashik")
+print(pythonset & dascset ,"are in both sets")
+print(pythonset - dascset ,"are only in python")
+print(pythonset|dascset ,"all student in both courec  ")
+cource_dict={
+    "Python": len(pythonset),
+    "Data Science": len(dascset)
+}
+for cource,val in cource_dict.items():
+    print(cource,":",val)
+    
+    
+growth={key:value*2 for key,value in cource_dict.items()}
+print(growth)
+
+
+for course,value in growth.items():
+    print(course,":",value)
