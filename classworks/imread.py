64df4947a710c817000c7229de73d90a92486d22
Salm-a-n <salmanntechs@gmail.com>
2025-10-04 08:21:25 +0530

    Add image reading functionality to load and print binary data

---
+++ classworks/imread.py
@@ -0,0 +1,4 @@
+import os
+with open(r"C:\Users\salma\OneDrive\Desktop\ME\my bike.png","rb") as image:#specify the path or store the image on the same folder 
+    binary_formate=image.read()
+print(binary_formate)
